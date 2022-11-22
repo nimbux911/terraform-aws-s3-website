b79ab89fa87f1e0bc0690ff6ba1fb017fd63f80f
Edgardo Quiroga <edgardo@nimbux911.com>
2022-11-22 15:17:40 -0300

    forcing local variable to be a set of strings

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   custom_subdomain = var.custom_subdomain == "" ? var.domain_name : "${var.custom_subdomain}.${var.domain_name}"
-  aliases = var.aliases != [] ? [for alias in toset(var.aliases): "${alias}.${var.domain_name}"] : []
+  aliases = var.aliases != [] ? toset([for alias in toset(var.aliases): "${alias}.${var.domain_name}"]) : []
 }
 
 #
