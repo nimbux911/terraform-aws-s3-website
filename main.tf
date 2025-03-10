7ebe1794f45592ef99135ad89ebd4ec6c2613f29
Giuliano Ursino <giuliano.ursino@nimbux911.com>
2025-03-10 17:07:50 -0300

    Reverted back to commit e49ef2c5

--- main.tf
+++ main.tf
@@ -143,30 +143,12 @@ resource "aws_cloudfront_distribution" "default" {
 #
 data "aws_iam_policy_document" "s3_policy" {
   statement {
-    actions   = ["s3:*"]
-    resources = [aws_s3_bucket.website.arn,"${aws_s3_bucket.website.arn}/*"]
-    sid       = "AllowS3ReaderRole"
-
-    principals {
-      type        = "AWS"
-      identifiers = [/*aws_cloudfront_origin_access_identity.default.iam_arn*/"arn:aws:iam::019101575375:role/ops-eks-worker"]
-    }
-  }
-  
-  statement {
-    effect    = "Allow"
     actions   = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.website.arn}/*"]
-    sid       = ""
 
     principals {
       type        = "AWS"
       identifiers = [aws_cloudfront_origin_access_identity.default.iam_arn]
-    }
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
     }
   }
 }
